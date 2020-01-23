9930bcc437f1cec72bf16354350b3d5b542ca738
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-01-23 06:44:10 +0000

    Automatically backed up by Learn

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,6 +3,6 @@ reqire 'rails_helper'
 describe Post do
   it 'has a summary' do
    post = Post.create!(title: "My title", description: "The post description")
-   expect(post.post_summary).to eq("")
+   expect(post.post_summary).to eq("My title - The post description")
   end
 end
